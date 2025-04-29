9aa4847061c2230f1b63dcc1dea0b402fb0d2873
Nigarselekk <nigarselek923@gmail.com>
2025-04-30 02:36:46 +0300

    Startup.cs update for UseHttpsRedirection

--- BookStore/WebApi/Startup.cs
+++ BookStore/WebApi/Startup.cs
@@ -30,30 +30,25 @@ namespace WebApi
                 options.UseInMemoryDatabase("BookStoreDb"));
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore API v1"));
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json","BookStore API v1"));
+        
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+        app.UseHttpsRedirection();
+    }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.UseAuthorization();
+    app.UseEndpoints(endpoints => endpoints.MapControllers());
+}
         }
     }
-}
